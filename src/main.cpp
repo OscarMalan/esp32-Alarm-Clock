df6d3210f9266283e341facb3b12d66335142792
Oscar Malan <Oscarmalan@icloud.com>
2023-07-31 12:02:18 +0100

    More efficient and Deep Sleep
    Reduced number of lines of code and added a deep sleep system to save power.
    TODO:
    Reduce nesting

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_GFX.h>
@@ -6,10 +7,12 @@
 
 RTC_DS3231 rtc;
 
-#define BUTTON_A 16
+#define BUTTON_A 4
 #define BUTTON_B 15
-#define BUTTON_C 14
+#define BUTTON_C 12
 #define Buzzer 17
+
+RTC_DATA_ATTR int bootCount = 0;
 
 #define WIRE Wire
 
@@ -22,72 +25,32 @@ int New_Hour = 0;
 int New_Minute = 1;
 int Count = 0;
 bool Done = false;
+char Text_Display[14][11] = {"Day", "Month", "Year", "Hour", "Minute", "Alarm", "Al1 Hour", "Al1 Minute", "Al2 Hour", "Al2 Minute", "Al3 Hour", "Al3 Minute", "Al4 Hour", "Al4 Minute"};
 char Week_Day[7][4] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
 int Alarm = 1;
-int Alarm1[2] = {6, 0};
-int Alarm2[2] = {6, 0};
-int Alarm3[2] = {6, 0};
-int Alarm4[2] = {6, 0};
-bool Alarm1ON = false;
-bool Alarm2ON = false;
-bool Alarm3ON = false;
-bool Alarm4ON = false;
+
+// By putting RTC_DATA_ATTR before defining a variable, it stores it in the RTC ram area on the ESP32, meaning if its modified it wont be forgotten when sleep mode is used
+RTC_DATA_ATTR int Alarm1[2] = {6, 0};
+RTC_DATA_ATTR int Alarm2[2] = {6, 0};
+RTC_DATA_ATTR int Alarm3[2] = {6, 0};
+RTC_DATA_ATTR int Alarm4[2] = {6, 0};
+RTC_DATA_ATTR bool Alarm1ON = false;
+RTC_DATA_ATTR bool Alarm2ON = false;
+RTC_DATA_ATTR bool Alarm3ON = false;
+RTC_DATA_ATTR bool Alarm4ON = false;
 
 
 void Variable_Value_Control(int &Changed_Varible, int Max_Value, int Min_Value, /* Handles text output on display, can't just parse the string as a paramenter due to how C++ handles strings */ int Text){
+  while (Done == false) {  
     display.clearDisplay();
     display.setCursor(0, 0);
     display.setTextSize(2);
-    // The best way I could figure out on how to handle this text, still more effiecent by 10 lines
-    switch (Text)
-    {
-    case 0:
-      display.print("Day");
-      break;
-    case 1:
-      display.print("Month");
-      break;
-    case 2:
-      display.print("Year");
-      break;
-    case 3:
-      display.print("Hour");
-      break;
-    case 4:
-      display.print("Minute");
-      break;
-    case 5:
-      display.print("Alarm");
-      break;
-    case 6:
-      display.print("Al1 Hour");
-      break;
-    case 7:
-      display.print("Al1 Minute");
-      break;
-    case 8:
-      display.print("Al2 Hour");
-      break;
-    case 9:
-      display.print("Al2 Minute");
-      break;
-    case 10:
-      display.print("Al3 Hour");
-      break;
-    case 11:
-      display.print("Al3 Minute");
-      break;
-    case 12:
-      display.print("Al4 Hour");
-      break;
-    case 13:
-      display.print("Al4 Minute");
-      break;
-    }
+    // Originally used switch case, but array is better
+    display.print(Text_Display[Text]);
     display.setCursor(0, 24);
     display.print(Changed_Varible);
     display.display();
-    if(digitalRead(BUTTON_B) == LOW){
+    if(digitalRead(BUTTON_B) == HIGH){
       if (Changed_Varible >= Max_Value){Changed_Varible = Min_Value; }
       // Count is used as a scaling, so that the longer the button is held down for, the larger the counting incremints
       else{if (Count >= 5) {Changed_Varible += 2;}
@@ -98,7 +61,7 @@ void Variable_Value_Control(int &Changed_Varible, int Max_Value, int Min_Value, 
       Count++;
     delay(100);
     }
-    else if(digitalRead(BUTTON_C) == LOW){
+    else if(digitalRead(BUTTON_C) == HIGH){
       if (Changed_Varible <= Min_Value){Changed_Varible = Max_Value;}
       else{if (Count >= 5) {Changed_Varible -= 2;}
       else if (Count >= 10) {Changed_Varible -= 5;}
@@ -108,42 +71,15 @@ void Variable_Value_Control(int &Changed_Varible, int Max_Value, int Min_Value, 
       Count++;
     delay(100);
     }
-    else if (digitalRead(BUTTON_A) == LOW)
+    else if (digitalRead(BUTTON_A) == HIGH)
     {
       Done = true;
     }
     else {Count = 0;}
-}
-
-void setup() {
-
-  // Turns on display
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-
-  // Tests RTC is working, tells user if it is failing
-  if (! rtc.begin()) {
-    display.clearDisplay();
-    display.setTextColor(WHITE);
-    display.setCursor(0, 0);
-    display.setTextSize(2);
-    display.print("RTC FAILED");
-    display.display();
   }
-
-  //====================================================================================================================================
-  // Comment this out when final, otherwise will change time everytime power is lost
-  rtc.adjust(DateTime(__DATE__, __TIME__));
-  //====================================================================================================================================
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.setTextSize(2);
-
-  pinMode(BUTTON_A, INPUT_PULLUP);
-  pinMode(BUTTON_B, INPUT_PULLUP);
-  pinMode(BUTTON_C, INPUT_PULLUP);
-  pinMode(Buzzer, OUTPUT);
+  Done = false; 
 }
+
 
 void Set_Time() {
   display.clearDisplay();
@@ -152,32 +88,18 @@ void Set_Time() {
   display.print("Set Time");
   display.display();
   delay(1000);
-  while(Done == false){
-    Variable_Value_Control(New_Day, 31, 1, 0);
-  }
+  Variable_Value_Control(New_Day, 31, 1, 0);
   delay(1000);
-  Done = false;
-  while(Done == false){
-    Variable_Value_Control(New_Month, 12, 1, 1);
-  }
+  Variable_Value_Control(New_Month, 12, 1, 1);
   delay(1000);
-  Done = false;
-  while(Done == false){
-    Variable_Value_Control(New_Year, 2039, 2023, 2);
-  }
+  Variable_Value_Control(New_Year, 2039, 2023, 2);
   delay(1000);
-  Done = false;
-  while(Done == false){
-    Variable_Value_Control(New_Hour, 23, 0, 3);
-  }
+  Variable_Value_Control(New_Hour, 23, 0, 3);
   delay(1000);
-  Done = false;
-  while(Done == false){
-    Variable_Value_Control(New_Minute, 59, 0, 4);
-  }
-  Done = false;
+  Variable_Value_Control(New_Minute, 59, 0, 4);
   // Adjust the RTC time
   rtc.adjust(DateTime(New_Year, New_Month, New_Day, New_Hour, New_Minute, 0));
+  delay(1000);
 }
 
 void Set_Alarm(){
@@ -188,71 +110,27 @@ void Set_Alarm(){
   display.print("Set Alarm");
   display.display();
   delay(1000);
-  while(Done == false){
-    Variable_Value_Control(Alarm, 4, 1, 5);
-  }
-  Done = false;
+  Variable_Value_Control(Alarm, 4, 1, 5);
   display.clearDisplay();
   delay(1000);
   switch (Alarm)
   {
   case 1:
-    while (Done == false)
-    {
-      Variable_Value_Control(Alarm1[0], 23, 0, 6);
-    }
-    Done = false;
-    delay(1000);
-    while (Done == false)
-    {
-      Variable_Value_Control(Alarm1[1], 59, 0, 7);
-    }
-    Done = false;
-    delay(1000);
+    Variable_Value_Control(Alarm1[0], 23, 0, 6);
+    Variable_Value_Control(Alarm1[1], 59, 0, 7);
     break;
   case 2:
-    while (Done == false)
-    {
-      Variable_Value_Control(Alarm2[0], 23, 0, 8);
-    }
-    Done = false;
-    delay(1000);
-    while (Done == false)
-    {
-      Variable_Value_Control(Alarm2[1], 59, 0, 9);
-    }
-    Done = false;
-    delay(1000);
+    Variable_Value_Control(Alarm2[0], 23, 0, 8);
+    Variable_Value_Control(Alarm2[1], 59, 0, 9);
     break;
   case 3:
-   while (Done == false)
-    {
-      Variable_Value_Control(Alarm3[0], 23, 0, 10);
-    }
-    Done = false;
-    delay(1000);
-    while (Done == false)
-    {
-      Variable_Value_Control(Alarm3[1], 59, 0, 11);
-    }
-    Done = false;
-    delay(1000);
+    Variable_Value_Control(Alarm3[0], 23, 0, 10);
+    Variable_Value_Control(Alarm3[1], 59, 0, 11);
     break;
   case 4:
-   while (Done == false)
-    {
-      Variable_Value_Control(Alarm4[0], 23, 0, 12);
-    }
-    Done = false;
-    delay(1000);
-    while (Done == false)
-    {
-      Variable_Value_Control(Alarm4[1], 59, 0, 13);
-    }
-    Done = false;
-    delay(1000);
+    Variable_Value_Control(Alarm4[0], 23, 0, 12);
+    Variable_Value_Control(Alarm4[1], 59, 0, 13);
     break;
-
   default:
     display.clearDisplay();
     display.setCursor(0, 0);
@@ -261,6 +139,7 @@ void Set_Alarm(){
     display.display();
     break;
   }
+  delay(1000);
 }
 
 void Control_Alarm(){
@@ -271,33 +150,22 @@ void Control_Alarm(){
   display.print("ON/OFF Al");
   display.display();
   delay(1000);
-  while(Done == false){
-    Variable_Value_Control(Alarm, 4, 1, 5);
-  }
-  Done = false;
+  Variable_Value_Control(Alarm, 4, 1, 5);
   switch (Alarm)
   {
   case 1:
-    if (Alarm1ON == false){Alarm1ON = true;}
-    else if (Alarm1ON == true){Alarm1ON = false;}
-    else {Alarm1ON = false;}
+    // ! infront of a bool variable makes it the opposite value, so then just assign using =
+    Alarm1ON = !Alarm1ON;
     break;
   case 2:
-    if (Alarm2ON == false){Alarm2ON = true;}
-    else if (Alarm2ON == true){Alarm2ON = false;}
-    else {Alarm2ON = false;}
+    Alarm2ON = !Alarm2ON;
     break;
   case 3:
-    if (Alarm3ON == false){Alarm3ON = true;}
-    else if (Alarm3ON == true){Alarm3ON = false;}
-    else {Alarm3ON = false;}
+    Alarm3ON = !Alarm3ON;
     break;
   case 4:
-    if (Alarm4ON == false){Alarm4ON = true;}
-    else if (Alarm4ON == true){Alarm4ON = false;}
-    else {Alarm4ON = false;}
+    Alarm4ON = !Alarm4ON;
     break;
-  
   default:
     display.clearDisplay();
     display.setCursor(0, 0);
@@ -306,13 +174,47 @@ void Control_Alarm(){
     display.display();
     break;
   }
+  delay(1000);
+}
+
+void setup() {
+
+  // Turns on display
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.setTextColor(WHITE);
+
+  // Tests RTC is working, tells user if it is failing
+  if (! rtc.begin()) {
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.setTextSize(2);
+    display.print("RTC FAILED");
+    display.display();
+  }
+
+
+  //====================================================================================================================================
+  // Comment this out when final, otherwise will change time everytime power is lost and sleep mode won't work
+  // rtc.adjust(DateTime(__DATE__, __TIME__));
+  //====================================================================================================================================
+
+  // Changed from 
+  pinMode(BUTTON_A, INPUT_PULLDOWN);
+  pinMode(BUTTON_B, INPUT_PULLDOWN);
+  pinMode(BUTTON_C, INPUT_PULLDOWN);
+  pinMode(Buzzer, OUTPUT);
+  // Tells esp32 to wake up from deep sleep after 59.999999 seconds
+  esp_sleep_enable_timer_wakeup(59999999);
+  // Will wake up esp32 from sleep when any of the buttons are pressed
+  esp_sleep_enable_ext1_wakeup(0x000000010,ESP_EXT1_WAKEUP_ANY_HIGH);
+  esp_sleep_enable_ext1_wakeup(0x000008000,ESP_EXT1_WAKEUP_ANY_HIGH);
+  esp_sleep_enable_ext1_wakeup(0x000001000,ESP_EXT1_WAKEUP_ANY_HIGH);
 }
 
 void loop() {
   DateTime now = rtc.now();
   
   display.clearDisplay();
-  delay(1000);
   display.setCursor(0, 0);
   display.setTextSize(2);
 
@@ -320,7 +222,7 @@ void loop() {
   display.print(Week_Day[now.dayOfTheWeek()]); display.setCursor(42, 0);
   display.print(now.day()); display.setTextSize(1); display.print("/"); display.setTextSize(2); display.print(now.month()); display.setTextSize(1); display.print("/"); display.setTextSize(2); display.println(now.year()-2000);
   display.setCursor(0, 24);
-  display.print(now.hour()); display.print(":"); display.print(now.minute()); display.print(":"); display.print(now.second()); display.setTextSize(1); display.print(" "); display.print(int(rtc.getTemperature())); display.print(" C");
+  display.print(now.hour()); display.print(":"); display.print(now.minute()); display.print("   ");display.print(int(rtc.getTemperature())); display.setTextSize(1); display.print("C");
   
   // Alarm ON/OFF display
   display.setTextSize(1);
@@ -346,13 +248,13 @@ void loop() {
   display.setCursor(100, 54);
   display.print("AL4");
 
-  if(digitalRead(BUTTON_A) == LOW){
+  if(digitalRead(BUTTON_A) == HIGH){
     Set_Time();
   }
-  if(digitalRead(BUTTON_B) == LOW){
+  if(digitalRead(BUTTON_B) == HIGH){
     Set_Alarm();
   }
-  if(digitalRead(BUTTON_C) == LOW){
+  if(digitalRead(BUTTON_C) == HIGH){
     Control_Alarm();
   }
 
@@ -386,6 +288,7 @@ void loop() {
       }
     }
   }
-
   display.display();
+  // Send ESP32 into deep sleep
+  esp_deep_sleep_start();
 }
